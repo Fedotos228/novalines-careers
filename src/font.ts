3ab59d3fbf46b144b6cf77b5f40a7a77b9321739
Fedotos228 <tudor.onceanu@gmail.com>
2024-04-11 15:23:26 +0300

    verneac precis

--- src/font.ts
+++ src/font.ts
@@ -3,20 +3,20 @@ import localFont from 'next/font/local'
 export const DinNextLtProFont = localFont({
   src: [
     {
-      path: './public/font/DINNEXTLTPRO-BOLD.otf',
+      path: '../public/font/DINNEXTLTPRO-BOLD.otf',
       weight: '700',
     },
     {
-      path: './public/font/DINNEXTLTPRO-BOLDITALIC.otf',
+      path: '../public/font/DINNEXTLTPRO-BOLDITALIC.otf',
       weight: '700',
       style: 'italic',
     },
     {
-      path: './public/font/DINNEXTLTPRO-MEDIUM.otf',
+      path: '../public/font/DINNEXTLTPRO-MEDIUM.otf',
       weight: '500',
     },
     {
-      path: './public/font/DINNEXTLTPRO-REGULAR.otf',
+      path: '../public/font/DINNEXTLTPRO-REGULAR.otf',
       weight: '400',
     },
   ],
