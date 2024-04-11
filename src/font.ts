cb960d515530f98ac74de8e69c52b9a3e6bcc06a
Fedotos228 <tudor.onceanu@gmail.com>
2024-04-11 15:18:48 +0300

    one more time

--- src/font.ts
+++ src/font.ts
@@ -3,20 +3,20 @@ import localFont from 'next/font/local'
 export const DinNextLtProFont = localFont({
   src: [
     {
-      path: '../public/font/DINNEXTLTPRO-BOLD.otf',
+      path: './font/DINNEXTLTPRO-BOLD.otf',
       weight: '700',
     },
     {
-      path: '../public/font/DINNEXTLTPRO-BOLDITALIC.otf',
+      path: './font/DINNEXTLTPRO-BOLDITALIC.otf',
       weight: '700',
       style: 'italic',
     },
     {
-      path: '../public/font/DINNEXTLTPRO-MEDIUM.otf',
+      path: './font/DINNEXTLTPRO-MEDIUM.otf',
       weight: '500',
     },
     {
-      path: '../public/font/DINNEXTLTPRO-REGULAR.otf',
+      path: './font/DINNEXTLTPRO-REGULAR.otf',
       weight: '400',
     },
   ],
