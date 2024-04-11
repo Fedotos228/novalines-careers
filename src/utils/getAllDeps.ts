a91b1181f57ce8d986ea96738961019aad7497b8
Fedotos228 <tudor.onceanu@gmail.com>
2024-04-11 13:20:22 +0300

    fixing Failed to fetch data

--- src/utils/getAllDeps.ts
+++ src/utils/getAllDeps.ts
@@ -3,6 +3,9 @@ const token = process.env.NEXT_PUBLIC_STRAPI_TOKEN
 export default async function getAllDeps() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/departaments?fields[0]=title&fields[1]=desc`)
 
-  if (!res.ok) throw new Error("Failed to fetch data")
+  if (!res.ok) {
+    console.log(res)
+    throw new Error("Failed to fetch data")
+  }
   return res.json()
 }
