2b5c9ab924955d6db214c251f4beb5479fd0c1ed
Fedotos228 <tudor.onceanu@gmail.com>
2024-04-11 16:00:11 +0300

    poate o sa mearga acum fontu

--- src/font.ts
+++ src/font.ts
@@ -1,24 +1,3 @@
 import localFont from 'next/font/local'
 
-export const DinNextLtProFont = localFont({
-  src: [
-    {
-      path: '../public/font/DINNEXTLTPRO-Bolt.otf',
-      weight: '700',
-    },
-    {
-      path: '../public/font/DINNEXTLTPRO-BoltItalic.otf',
-      weight: '700',
-      style: 'italic',
-    },
-    {
-      path: '../public/font/DINNEXTLTPRO-Medium.otf',
-      weight: '500',
-    },
-    {
-      path: '../public/font/DINNEXTLTPRO-Regular.otf',
-      weight: '400',
-    },
-  ],
-  variable: '--font-din-next-lt-pro',
-})
+export 
