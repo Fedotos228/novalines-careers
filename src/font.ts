3434c397bf63c267595fcb3371e752683d4ce233
Fedotos228 <tudor.onceanu@gmail.com>
2024-04-11 15:25:00 +0300

    SA MI FUT UNA DACA NU MERGE ACUM

--- src/font.ts
+++ src/font.ts
@@ -3,20 +3,20 @@ import localFont from 'next/font/local'
 export const DinNextLtProFont = localFont({
   src: [
     {
-      path: '../public/font/DINNEXTLTPRO-BOLD.otf',
+      path: '../public/font/DINNEXTLTPRO-BOLD.OTF',
       weight: '700',
     },
     {
-      path: '../public/font/DINNEXTLTPRO-BOLDITALIC.otf',
+      path: '../public/font/DINNEXTLTPRO-BOLDITALIC.OTF',
       weight: '700',
       style: 'italic',
     },
     {
-      path: '../public/font/DINNEXTLTPRO-MEDIUM.otf',
+      path: '../public/font/DINNEXTLTPRO-MEDIUM.OTF',
       weight: '500',
     },
     {
-      path: '../public/font/DINNEXTLTPRO-REGULAR.otf',
+      path: '../public/font/DINNEXTLTPRO-REGULAR.OTF',
       weight: '400',
     },
   ],
