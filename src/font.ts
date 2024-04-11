0b6dca38f1f86a98aefdffdcfcdb2a25b047251a
Fedotos228 <tudor.onceanu@gmail.com>
2024-04-11 15:27:12 +0300

    off doare

--- src/font.ts
+++ src/font.ts
@@ -3,20 +3,20 @@ import localFont from 'next/font/local'
 export const DinNextLtProFont = localFont({
   src: [
     {
-      path: '../public/font/DINNEXTLTPRO-BOLD.OTF',
+      path: '../public/font/DINNEXTLTPRO-BOLD.otf',
       weight: '700',
     },
     {
-      path: '../public/font/DINNEXTLTPRO-BOLDITALIC.OTF',
+      path: '../public/font/DINNEXTLTPRO-BOLDITALIC.otf',
       weight: '700',
       style: 'italic',
     },
     {
-      path: '../public/font/DINNEXTLTPRO-MEDIUM.OTF',
+      path: '../public/font/DINNEXTLTPRO-MEDIUM.otf',
       weight: '500',
     },
     {
-      path: '../public/font/DINNEXTLTPRO-REGULAR.OTF',
+      path: '../public/font/DINNEXTLTPRO-REGULAR.otf',
       weight: '400',
     },
   ],
